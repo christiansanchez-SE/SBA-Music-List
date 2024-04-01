d0fa60261433fea41c2b77e911890c9a05da19f1
christiansanchez-SE <christiansanchez714@gmail.com>
2024-03-31 23:44:00 -0400

    Second Commit

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-//
+// This is a toggle that turns from bright to dark mode
 
 const body = document.querySelector("body");
 const btn = document.getElementById("btn");
@@ -7,16 +7,16 @@ function toggleBtn() {
     btn.classList.toggle("active");
     if (btn.classList.contains("active")) {
         body.style.background = "rgb(20, 21, 21)";
-        body.style.color = "white";
+        body.style.color = "hsl(220, 3%, 81%)";
         body.style.transition = "2s";
     } else {
-        body.style.background = "white";
+        body.style.background = "hsl(220, 3%, 81%)";
         body.style.color = "rgb(20, 21, 21)";
         body.style.transition = "2s";
     }
 }
 
-// This is the logic behind the to-do list
+// This is the logic behind the Music list
 
 const inputBox = document.getElementById("input-box");
 const listContainer = document.getElementById("list-container");
@@ -31,6 +31,7 @@ function addTask(){
         li.innerHTML = inputBox.value;
         listContainer.appendChild(li);
         let span = document.createElement("span");
+        // This is the X-out button
         span.innerHTML = "\u00d7"
         li.appendChild(span); 
     }
@@ -45,3 +46,12 @@ listContainer.addEventListener("click", function(event){
         event.target.parentElement.remove();
     }
 });
+
+
+/////////////////
+
+function toggleButton(){
+    window.scrollTo({top: 0, behavior:'smooth'});
+}
+
+window.alert("Hey Welcome To My Page, Its Still In Production!");
